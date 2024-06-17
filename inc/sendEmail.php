69facff383f1b724a230b99be790ccc34d9657e4
SamuelBarb0 <sambarbosa66@gmail.com>
2024-06-17 16:10:16 -0500

    Cambio 2

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -3,67 +3,61 @@
 // Replace this with your own email address
 $siteOwnersEmail = 'sambarbosa66@gmail.com';
 
+$errors = [];
+$message = '';
 
-if($_GET) {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Sanitize input data
+    $name = htmlspecialchars(trim($_POST['contactName']));
+    $email = htmlspecialchars(trim($_POST['contactEmail']));
+    $subject = htmlspecialchars(trim($_POST['contactSubject']));
+    $contact_message = htmlspecialchars(trim($_POST['contactMessage']));
 
-   $name = trim(stripslashes($_GET['contactName']));
-   $email = trim(stripslashes($_GET['contactEmail']));
-   $subject = trim(stripslashes($_GET['contactSubject']));
-   $contact_message = trim(stripslashes($_GET['contactMessage']));
+    // Validate inputs
+    if (strlen($name) < 2) {
+        $errors['name'] = "Por favor, introduce tu nombre.";
+    }
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors['email'] = "Por favor, introduce un email válido.";
+    }
+    if (strlen($contact_message) < 15) {
+        $errors['message'] = "Por favor, introduce tu mensaje. Debe tener al menos 15 caracteres.";
+    }
+    // If subject is empty, use default
+    if (empty($subject)) {
+        $subject = "Formulario de Contacto";
+    }
 
-   // Check Name
-	if (strlen($name) < 2) {
-		$error['name'] = "Porfavor, introduce tu nombre.";
-	}
-	// Check Email
-	if (!preg_match('/^[a-z0-9&\'\.\-_\+]+@[a-z0-9\-]+\.([a-z0-9\-]+\.)*+[a-z]{2}/is', $email)) {
-		$error['email'] = "Porfavor, introduce un email valido.";
-	}
-	// Check Message
-	if (strlen($contact_message) < 15) {
-		$error['message'] = "Porfavor, introduce tu mensaje, debe tener al menos 15 caracteres.";
-	}
-   // Subject
-	if ($subject == '') { $subject = "Formulario de Contacto"; }
+    // Build email message
+    $message .= "Email de: " . $name . "<br />";
+    $message .= "Dirección de Email: " . $email . "<br />";
+    $message .= "Mensaje: <br />";
+    $message .= $contact_message;
+    $message .= "<br /> ----- <br /> Este email ha sido enviado desde tu formulario de contacto. <br />";
 
+    // Set From: header
+    $from = $name . " <" . $email . ">";
 
-   // Set Message
-   $message .= "Email de: " . $name . "<br />";
-	$message .= "Direccion de Email: " . $email . "<br />";
-   $message .= "Mensaje: <br />";
-   $message .= $contact_message;
-   $message .= "<br /> ----- <br /> Este email ha sido enviado desde tu formulario de contacto. <br />";
+    // Email headers
+    $headers = "From: " . $from . "\r\n";
+    $headers .= "Reply-To: " . $email . "\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-   // Set From: header
-   $from =  $name . " <" . $email . ">";
-
-   // Email Headers
-	$headers = "De: " . $from . "\r\n";
-	$headers .= "Responder a: ". $email . "\r\n";
- 	$headers .= "MIME-Version: 1.0\r\n";
-	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
-
-
-   if (!$error) {
-
-      ini_set("sendmail_from", $siteOwnersEmail); // for windows server
-      $mail = mail($siteOwnersEmail, $subject, $message, $headers);
-
-		if ($mail) { echo "OK"; }
-      else { echo "Algo ha ido mal, porfavor intentalo de nuevo"; }
-		
-	} # end if - no validation error
-
-	else {
-
-		$response = (isset($error['name'])) ? $error['name'] . "<br /> \n" : null;
-		$response .= (isset($error['email'])) ? $error['email'] . "<br /> \n" : null;
-		$response .= (isset($error['message'])) ? $error['message'] . "<br />" : null;
-		
-		echo $response;
-
-	} # end if - there was a validation error
-
+    // Send email if there are no errors
+    if (empty($errors)) {
+        if (mail($siteOwnersEmail, $subject, $message, $headers)) {
+            echo "OK"; // Informa al cliente que el correo se envió correctamente
+        } else {
+            echo "Algo ha ido mal, por favor inténtalo de nuevo."; // Informa al cliente que hubo un problema al enviar el correo
+        }
+    } else {
+        // If there are validation errors, construct and echo the error response
+        $response = '';
+        foreach ($errors as $error) {
+            $response .= $error . "<br />";
+        }
+        echo $response;
+    }
 }
-
 ?>
